949f4621ec962c32513f0d5f0f5bd49efd57f679
rajroushan6714 <rajubhai6714x@gmail.com>
2023-07-25 19:48:42 +0530

    use of map function and the scopes of functions

--- js 3/index.js
+++ js 3/index.js
@@ -3,3 +3,11 @@ const output = document.querySelector("#output");
 // instead of defining a separate function, we can pass an anonymous function in 'addEventListener()'.
 // and passed the event as it's argument.
 textBox.addEventListener("keydown", function (event) {output.textContent = `You pressed "${event.key}".`;});
+// you could even pass arrow function instead of anonymous function. Replace "function (event)" to "(event) =>"
+
+function temp() {
+    const originals = [1, 2 , 3];
+    const doubled = originals.map(item => item * 2);
+    console.log(doubled);
+}
+temp();
